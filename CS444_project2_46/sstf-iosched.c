ecb3f61faf0e2e0250718cb7ad73abf3c13dfbca
Jason Ye <yeja@oregonstate.edu>
2017-10-18 23:12:34 -0700

    added and updated functions
    started implementing merge and dispatcher functions...still have a lot to do

--- CS444_project2_46/sstf-iosched.c
+++ CS444_project2_46/sstf-iosched.c
@@ -19,7 +19,22 @@ struct sstf_data{
 
 static struct request *
 
-struct void sstf_merged_request(){
+struct void sstf_merged_request(struct request_queue *q, struct request *rq, struct request *next){
+	list_del_init(&next->queueList);
+}
+
+sstf_add(struct, struct){
+
+}
+
+sstf_dispatcher(){
+	struct sstf_data *sd = q->elevator->elevator_data;
+
+	if (!list_empty(&sd->queue)){
+
+
+
+	}
 
 }
 
@@ -36,13 +51,7 @@ sstf_initializer_request(struct, struct){
 
 }
 
-sstf_dispatcher(){
 
-}
-
-sstf_add(struct, struct){
-
-}
 
 sstf_exit(){
 
