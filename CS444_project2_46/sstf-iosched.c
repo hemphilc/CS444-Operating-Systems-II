0eff62509f5a40b062bbd59227a8a4b2121d270e
Jason Ye <yeja@oregonstate.edu>
2017-10-18 22:14:43 -0700

    sstf-iosched
    added prototype functions....still have a lot to do

--- CS444_project2_46/sstf-iosched.c
+++ CS444_project2_46/sstf-iosched.c
@@ -1,16 +1,53 @@
 /*
  * Jason Ye - yeja@oregonstate.edu
  * Corey Hemphill - hemphilc@oregonstate.edu
- * CS444 - Concurrency Problem 1
- * October 9, 2017
- * concurrency.c
+ * CS444 - Project 2
+ * October 18, 2017
+ * sstf-ioched.c
 */
 
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
+#include <linux/bio.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+
+struct sstf_data{
+	struct list_head queue;
 
 
+};
+
+struct void sstf_merged_request(struct , struct){
+
+}
+
+static struct request *
+sstf_latter_request(struct, struct){
+
+}
+
+sstf_former_request(struct, struct){
+
+}
+
+sstf_initializer_request(struct, struct){
 
 
+}
 
+sstf_dispatcher(){
+
+}
+
+sstf_add(struct, struct){
+
+}
+
+sstf_exit(){
+
+}
 
 
 
