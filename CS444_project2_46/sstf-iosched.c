964f84a823856c62f72818d201e78b2f73198882
Corey Hemphill <hemphilc@oregonstate.edu>
2017-10-20 22:27:10 -0700

    v1.0

--- CS444_project2_46/sstf-iosched.c
+++ CS444_project2_46/sstf-iosched.c
@@ -1,11 +1,14 @@
 /*
  * Jason Ye - yeja@oregonstate.edu
  * Corey Hemphill - hemphilc@oregonstate.edu
- * CS444 - Project 2
- * October 18, 2017
- * sstf-ioched.c
+ * CS444 - Homework 2 - sstf I/O Scheduler
+ * October 23, 2017
+ * sstf-iosched.c
 */
 
+/*
+ * elevator noop
+ */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
@@ -13,56 +16,145 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-struct sstf_data{
-	struct list_head queue;
+#define DEBUG 0
+
+struct sstf_data {
+        struct list_head queue;
+
 };
 
+static void noop_merged_requests(struct request_queue *q, struct request *rq,
+                                 struct request *next)
+{
+        list_del_init(&next->queuelist);
+}
+
+static int sstf_dispatch(struct request_queue *q, int force)
+{
+        struct sstf_data *sd = q->elevator->elevator_data;
+
+        if (!list_empty(&sd->queue)) {
+                struct request *rq, prq, nrq;
+
+
+
+
+
+
+
+
+
+                rq = list_entry(sd->queue.next, struct request, queuelist);
+                list_del_init(&rq->queuelist);
+                elv_dispatch_sort(q, rq);
+                return 1;
+        }
+        return 0;
+}
+
+static void sstf_add_request(struct request_queue *q, struct request *rq)
+{
+        struct sstf_data *sd = q->elevator->elevator_data;
+
+        list_add_tail(&rq->queuelist, &sd->queue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 static struct request *
+noop_former_request(struct request_queue *q, struct request *rq)
+{
+        struct noop_data *nd = q->elevator->elevator_data;
 
-struct void sstf_merged_request(struct request_queue *q, struct request *rq, struct request *next){
-	list_del_init(&next->queueList);
+        if (rq->queuelist.prev == &nd->queue)
+                return NULL;
+        return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
-sstf_add(struct, struct){
+static struct request *
+noop_latter_request(struct request_queue *q, struct request *rq)
+{
+        struct noop_data *nd = q->elevator->elevator_data;
 
+        if (rq->queuelist.next == &nd->queue)
+                return NULL;
+        return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-sstf_dispatcher(){
-	struct sstf_data *sd = q->elevator->elevator_data;
+static int noop_init_queue(struct request_queue *q, struct elevator_type *e)
+{
+        struct noop_data *nd;
+        struct elevator_queue *eq;
 
-	if (!list_empty(&sd->queue)){
+        eq = elevator_alloc(q, e);
+        if (!eq)
+                return -ENOMEM;
 
+        nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
+        if (!nd) {
+                kobject_put(&eq->kobj);
+                return -ENOMEM;
+        }
+        eq->elevator_data = nd;
 
+        INIT_LIST_HEAD(&nd->queue);
 
-	}
-
+        spin_lock_irq(q->queue_lock);
+        q->elevator = eq;
+        spin_unlock_irq(q->queue_lock);
+        return 0;
 }
 
-sstf_latter_request(struct, struct){
+static void noop_exit_queue(struct elevator_queue *e)
+{
+        struct noop_data *nd = e->elevator_data;
 
+        BUG_ON(!list_empty(&nd->queue));
+        kfree(nd);
 }
 
-sstf_former_request(struct, struct){
+static struct elevator_type elevator_noop = {
+        .ops = {
+                .elevator_merge_req_fn          = noop_merged_requests,
+                .elevator_dispatch_fn           = sstf_dispatch,
+                .elevator_add_req_fn            = sstf_add_request,
+                .elevator_former_req_fn         = noop_former_request,
+                .elevator_latter_req_fn         = noop_latter_request,
+                .elevator_init_fn               = noop_init_queue,
+                .elevator_exit_fn               = noop_exit_queue,
+        },
+        .elevator_name = "sstf",
+        .elevator_owner = THIS_MODULE,
+};
 
+static int __init sstf_init(void)
+{
+        return elv_register(&elevator_sstf);
 }
 
-sstf_initializer_request(struct, struct){
-
-
+static void __exit sstf_exit(void)
+{
+        elv_unregister(&elevator_sstf);
 }
 
+module_init(noop_init);
+module_exit(noop_exit);
 
 
-sstf_exit(){
-
-}
-
-
-
-
-
-
-
-
-
-
+MODULE_AUTHOR("Jason Ye, Corey Hemphill");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("sstf IO scheduler");
