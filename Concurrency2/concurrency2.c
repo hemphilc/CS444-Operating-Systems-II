d349c6df66edf1fb922d8ad3391d0ff149d45d0d
Corey Hemphill <hemphilc@oregonstate.edu>
2017-10-27 21:35:38 -0700

    v1.1.0

--- Concurrency2/concurrency2.c
+++ Concurrency2/concurrency2.c
@@ -11,141 +11,158 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define TRUE  1
+#define TRUE 1
 #define NUM_PHILOSOPHERS 5
+#define NAME_LEN 20
+#define THINK_TIME 20
+#define EAT_TIME 8
 
 pthread_mutex_t print_lock;
-pthread_mutex_t fork[NUM_PHILOSOPHERS];
+pthread_mutex_t forks[NUM_PHILOSOPHERS];
 
-struct philosopher_info{
-	char name[100];
+struct philosopher_info {
+	char name[NAME_LEN];
 	int seat_num;
 };
 
+void think(struct philosopher_info *philosopher);
+void eat(struct philosopher_info *philosopher);
+void get_forks(struct philosopher_info *philosopher);
+void put_forks(struct philosopher_info *philosopher);
+void *philosopher_action(void *philosopher);
 
-void philosopher_names(struct philosopher_info *person){
+int main() {
+	struct philosopher_info *philosophers;
+	//five philosophers will be on the table
+	philosophers = malloc(sizeof(struct philosopher_info) * NUM_PHILOSOPHERS);
 
-	person = malloc(sizeof(struct philosopher_info) * NUM_PHILOSOPHERS);
+	//clear all name buffers
+	memset(philosophers[0].name, '\0', sizeof(philosophers[0].name));
+        memset(philosophers[1].name, '\0', sizeof(philosophers[1].name));
+        memset(philosophers[2].name, '\0', sizeof(philosophers[2].name));
+        memset(philosophers[3].name, '\0', sizeof(philosophers[3].name));
+        memset(philosophers[4].name, '\0', sizeof(philosophers[4].name));
 
-	//delcaring their names
-	strcpy(person[0].name, "Plato";);
-	strcpy(person[1].name, "Aristotle";);
-	strcpy(person[2].name, "Epicurus";);
-	strcpy(person[3].name, "Parmenides";);
-	strcpy(person[4].name, "Socrates";);
-}
+	//assign philosopher names
+	strcpy(philosophers[0].name, "Plato");
+        strcpy(philosophers[1].name, "Aristotle");
+        strcpy(philosophers[2].name, "Epicurus");
+        strcpy(philosophers[3].name, "Parmenides");
+        strcpy(philosophers[4].name, "Socrates"); //pronounced "So-Crates"
 
-void think(struct philosopher_info *person){
-	
-	pthread_mutex_lock(&print_lock);
-	printf("%s is thinking... \n", person->name);
-	pthread_mutex_unlock(&print_lock);
-	sleep(rand()%20+1);	
-	printf("%s finished thinking... \n", person->name);
-}
-
-void eat(struct philosopher_info *person){
-	pthread_mutex_lock(&print_lock);
-	printf("%s is eating... \n", person->name);
-	pthread_mutex_unlock(&print_lock);
-	sleep(rand()%8+2);
-	
-	pthread_mutex_lock(&print_lock);
-	printf("%s finishes eating... \n", person->name);
-	pthread_mutex_unlock(&print_lock);
-	
-}
-
-void get_forks(struct philosopher_info *person){
-
-	int right_fork;
-	int left_fork;
-
-	if (person->seat_num + 1 == NUM_PHILOSOPHERS){
-		right_fork = 0;
-		left_fork = person->seat_num;
-	}
-	else{
-		right_fork = person->seat_num;
-		left_fork = person->seat_num + 1;
-	}
-	//first fork
-	phtread_mutex_lock(&print_lock);
-	printf("Fork #%d assign to %s\n", right_fork, person->name);
-	phtread_mutex_unlock(&print_lock);
-	phtread_mutex_lock(&fork[right_fork]);
-
-	//second fork
-	phtread_mutex_lock(&print_lock);
-	printf("Fork #%d assign to %s\n", left_fork, person->name);
-	phtread_mutex_unlock(&print_lock);
-	phtread_mutex_lock(&fork[left_fork]);
-
-}
-
-void put_forks(struct philosopher_info *person){
-	int right_fork;
-	int left_fork;
-	if(person->seat_num+1 == NUM_PHILOSOPHERS){
-		right_fork = 0;
-		left_fork = person->seat_num;
-	}
-	else{
-		right_fork = person->seat_num;
-		left_fork = person->seat_num+1;
-	}
-
-	pthread_mutex_lock(&print_lock);
-	printf("%s put down the forks... \n", person->name);
-	pthread_mutex_unlock(&print_lock);
-	pthread_mutex_unlock(&forks[right_fork);
-	pthread_mutex_unlock(&forks[left_fork);
-	
-}
-
-void actions(void *philosopher){
-
-	//assigning each philosopher's actions
-	struct philosopher_info *person = (struct philosopher_info *)philosopher;
-	
-	while (TRUE){
-		think(person);
-		get_fork(person);
-		eat(person);
-		put_forks(person);
-	}
-	
-}
-
-int main(){
-
-	struct philosopher_info *person;
-	//five philosohper will be on the table
-	person = malloc(sizeof(struct philosopher_info) * NUM_PHILOSOPHERS);
-
-	philosopher_names(person);
-
-	//assign each person a seat
 	int i;
-	
-	for (i = 0; i < NUM_PHILOSOPHERS; i++){
-		person[i].seat_num = i;
+	//assign each philosopher a seat
+	for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+		philosophers[i].seat_num = i;
 	}
 
 	//mutex initialization
 	pthread_mutex_init(&print_lock, NULL);
-	for (i = 0; i < NUM_PHILOSOPHERS; i++){
-		pthread_mutex_init(&fork[i], NULL);
+	for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+		pthread_mutex_init(&forks[i], NULL);
 	}
 
+	//thread creation
 	pthread_t threads[NUM_PHILOSOPHERS];
-	for (i = 0; i < NUM_PHILOSOPHERS; i++){
-		pthread_create(&threads[i], NULL, actions, &person[i]);
+	for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+		pthread_create(&threads[i], NULL, philosopher_action, &philosophers[i]);
 	}
 
-	for (i = 0; i < NUM_PHILOSOPHERS; i++){
+	//thread joining
+	for (i = 0; i < NUM_PHILOSOPHERS; i++) {
 		pthread_join(threads[i], NULL);
 	}
 
 	return 0;
+}
+
+void think(struct philosopher_info *philosopher) {
+	pthread_mutex_lock(&print_lock);
+	
+	printf("%s is thinking...\n", philosopher->name);
+	
+	pthread_mutex_unlock(&print_lock);
+	
+	sleep((rand() % THINK_TIME) + 1);	
+	
+	printf("%s finished thinking...\n", philosopher->name);
+}
+
+void eat(struct philosopher_info *philosopher) {
+	pthread_mutex_lock(&print_lock);
+
+	printf("%s is eating...\n", philosopher->name);
+	
+	pthread_mutex_unlock(&print_lock);
+	
+	sleep(rand() % EAT_TIME + 2);
+	
+	pthread_mutex_lock(&print_lock);
+	
+	printf("%s finished eating...\n", philosopher->name);
+	
+	pthread_mutex_unlock(&print_lock);
+}
+
+void get_forks(struct philosopher_info *philosopher) {
+	int right_fork;
+	int left_fork;
+
+	if ((philosopher->seat_num + 1) == NUM_PHILOSOPHERS) {
+		right_fork = 0;
+
+		left_fork = philosopher->seat_num;
+	}
+	else {
+		right_fork = philosopher->seat_num;
+		
+		left_fork = philosopher->seat_num + 1;
+	}
+
+	//right fork
+	pthread_mutex_lock(&print_lock);
+	printf("Fork #%d assigned to %s\n", right_fork, philosopher->name);
+	pthread_mutex_unlock(&print_lock);
+	pthread_mutex_lock(&forks[right_fork]);
+
+	//left fork
+	pthread_mutex_lock(&print_lock);
+	printf("Fork #%d assigned to %s\n", left_fork, philosopher->name);
+	pthread_mutex_unlock(&print_lock);
+	pthread_mutex_lock(&forks[left_fork]);
+}
+
+void put_forks(struct philosopher_info *philosopher) {
+	int right_fork;
+	int left_fork;
+
+	if (philosopher->seat_num + 1 == NUM_PHILOSOPHERS) {
+		right_fork = 0;
+
+		left_fork = philosopher->seat_num;
+	}
+	else {
+		right_fork = philosopher->seat_num;
+
+		left_fork = philosopher->seat_num + 1;
+	}
+
+	pthread_mutex_lock(&print_lock);
+	printf("%s has put down the fork...\n", philosopher->name);
+	pthread_mutex_unlock(&print_lock);
+	pthread_mutex_unlock(&forks[right_fork]);
+	pthread_mutex_unlock(&forks[left_fork]);
+	
+}
+
+void *philosopher_action(void *philosopher) {
+	//assigning each philosopher's actions
+	struct philosopher_info *philosophers = (struct philosopher_info *)philosopher;
+	
+	while (TRUE) {
+		think(philosophers);
+		get_forks(philosophers);
+		eat(philosophers);
+		put_forks(philosophers);
+	}
 }
