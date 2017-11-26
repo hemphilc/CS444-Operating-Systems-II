d4e5ee819399c91951304f0c51c166c819e7ee14
Corey Hemphill <hemphilc@oregonstate.edu>
2017-11-25 16:56:17 -0800

    Added assignment head comments

--- CS444_project4_46/slob.c
+++ CS444_project4_46/slob.c
@@ -1,4 +1,12 @@
 /*
+ * Jason Ye - yeja@oregonstate.edu
+ * Corey Hemphill - hemphilc@oregonstate.edu
+ * CS444 - Project 4 - The SLOB SLAB
+ * November 26, 2017
+ * slob.c
+*/
+
+/*
  * SLOB Allocator: Simple List Of Blocks
  *
  * Matt Mackall <mpm@selenic.com> 12/30/03
